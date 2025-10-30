40db115887e3a9bd36ef3adfed205aded8a67e7e
Miguel A. Rozalen <mikelangel1993@gmail.com>
2025-10-30 20:12:38 +0100

    nuevas velas

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,4 +3,4 @@
 export const STANDARD_PACKAGING_COST = 1.99;
 export const PREMIUM_PACKAGING_COST_PER_ITEM = 3.99;
 export const SHIPPING_COST = 4.99;
-export const SELLER_EMAIL = "marivelas@gmail.com";
+export const SELLER_EMAIL = "marivelas.contact@gmail.com";
